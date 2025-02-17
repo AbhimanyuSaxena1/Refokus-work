ee402223cd19bc3f28ce36e49b9cc65f3f94a1b3
abhimanyusaxena1 <abhimanyusaxena1>
2025-02-18 04:20:51 +0530

    update Footer component for improved text styling and added Facebook link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,22 +6,22 @@ function Footer() {
       <div className='max-w-screen-xl mx-auto  select-none px-5'>
         <div id="upper" className='lg:flex lg:border-b-2  mb-4 lg:items-center lg:gap-40'>
           <div id='left' className='w-1/2'>
-            <h1 className='text-[20vw] lg:text-[13vw] uppercase lg:capitalize tracking-tight font-medium'>
+            <h1 className='text-[20vw] lg:text-[13vw] uppercase lg:capitalize  tracking-tight font-medium'>
               refokus.
             </h1>
           </div>
           <div id="right" className='flex flex-col-reverse   lg:flex-row gap-10  lg:gap-15 w-full lg:w-1/2'>
             <div id='pt1' className='flex pb-2  pt-2  lg:border-none justify-between  text-center lg:text-left lg:w-1/2 gap-5 lg:gap-10'>
               <div id="social" className='w-1/2 flex flex-col'>
-                <h1 className='font-medium opacity-60 mb-5'>Socials</h1>
-                {["Instagram", "Twitter", "LinkedIn"].map((elem) => (<a className='mt-2 opacity-60' href=''>{elem}</a>))}
+                <h1 className='font-medium text-zinc-400 text-[6vw] lg:text-xl mb-5'>Socials</h1>
+                {["Instagram", "Twitter", "LinkedIn","Facebook"].map((elem) => (<a className='mt-2 ' href=''>{elem}</a>))}
               </div>
-              <div id="Sitemap" className='w-1/2  border-l-2  flex flex-col'>
-                <h1 className='font-medium opacity-60 mb-5'>Sitemap</h1>
+              <div id="Sitemap" className='w-1/2  border-l-2 lg:border-none  flex flex-col'>
+                <h1 className='font-medium text-zinc-400 text-[6vw] lg:text-xl mb-5'>Sitemap</h1>
                 {["Home", "Work", "Careers", "Contact"].map((elem) => (<a className='mt-2' href=''>{elem}</a>))}
               </div>
             </div>
-            <div id="pt2" className='text-center  lg:text-right w-full lg:w-1/2 flex flex-col justify-between items-center   lg:items-end'>
+            <div id="pt2" className='text-center  lg:text-sm lg:mt-5 lg:text-right w-full lg:w-1/2 flex flex-col justify-between items-center   lg:items-end'>
               <p className='font-medium text-md'>Refokus is a pioneering digital agency driven by design and empowered by technology.</p>
               <img src="https://assets-global.website-files.com/5df3de8e749203dc3167a479/65369e818884afbae46a35fc_Webflow-badge.svg" className='w-40 h-20' alt="" />
             </div>
