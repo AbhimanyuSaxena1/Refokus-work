b8ce4cf80a629f8d870a7be0a08773a4df27ace9
abhimanyusaxena1 <abhimanyusaxena1>
2025-02-18 04:07:00 +0530

    add playsInline attribute to video for better compatibility

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -15,7 +15,7 @@ function Product({ live, Case, name, description, move, index,background }) {
       <div className={`mt-5 mr-5 ml-5 mb-5 max-w-screen-xl rounded-xl   lg:rounded-none lg:flex rounded-2xl items-center justify-between ${background} lg:bg-black text-white py-5 lg:py-0 px-10 lg:px-15 mx-auto h-[fit] lg:h-[18vw]`}>
 
         <div id='left' className='w-fit lg:w-[70vw] text-left lg:text-left font-semibold ml-1 text-2xl border-b-2 lg:border-none  lg:text-6xl'>{name}.</div>
-        <video src={videos[index]} autoPlay muted loop className='mt-5 h-[60vw] rounded-xl w-full flex lg:hidden'></video>
+        <video src={videos[index]} playsInline autoPlay muted loop className='mt-5 h-[60vw] rounded-xl w-full flex lg:hidden'></video>
         <div id='right' className='w-full mt-5 lg:mt-none lg:w-[30vw]  lg:flex flex-col'>
           <p>{description}</p>
           <div id='btn' className='flex gap-4 items-center justify-center whitespace-nowrap  mt-5'>
