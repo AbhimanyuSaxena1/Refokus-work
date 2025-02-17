cbbe32a6d8fefbcf84601b98236332be626c176e
abhimanyusaxena1 <abhimanyusaxena1>
2025-02-18 04:03:31 +0530

    removed right border for responsiveness

--- src/components/Strip.jsx
+++ src/components/Strip.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Strip({ number, url, index }) {
   return (
-    <div className={`w-full lg:w-[16.66%] ${index === 5 ? "border-r-0" : "border-r-2"} ${(index === 3 || index === 4 || index === 5) ? "hidden lg:flex" : "flex"} min-h-15 lg:min-h-20 items-center px-10 lg:px-5 justify-between border-zinc-800 lg:h-fit gap-10`}>
+    <div className={`w-full lg:w-[16.66%]  ${index === 5 ? "lg:border-r-0" : "lg:border-r-2"} ${(index === 3 || index === 4 || index === 5) ? "hidden lg:flex" : "flex"} min-h-15 lg:min-h-20 items-center px-10 lg:px-5 justify-between border-zinc-800 lg:h-fit gap-10`}>
       <img className='' src={url} alt="" />
       <h1 className='text-xl lg:text-lg font-base'>{number}</h1>
     </div>
